fad0345f144aafb16f79f9f7b0a52a7c6e33de58
Heriberto David <herdavyep@hotmail.com>
2019-02-23 23:07:31 -0500

    prueba prestamosJP

---
+++ precache-manifest.7fdc36eba2da53fb8ebec96ca8774a16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed67553305329fe6b43e745a0ce91e5b",
+    "url": "/static/media/marco.ed675533.png"
+  },
+  {
+    "revision": "2e54e51f0c5936576b364ee57350ab7a",
+    "url": "/static/media/grano-cafe.2e54e51f.png"
+  },
+  {
+    "revision": "fac159a300a5cd5efbd3c544cb37d3b0",
+    "url": "/static/media/dinero.fac159a3.png"
+  },
+  {
+    "revision": "2562376a7a31cb130abb6934f73fcfd1",
+    "url": "/static/media/Danger.2562376a.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c350c836d5b26d5ad49d",
+    "url": "/static/js/main.c350c836.chunk.js"
+  },
+  {
+    "revision": "c880174bdcadf760ff59",
+    "url": "/static/js/1.c880174b.chunk.js"
+  },
+  {
+    "revision": "c350c836d5b26d5ad49d",
+    "url": "/static/css/main.99c819f7.chunk.css"
+  },
+  {
+    "revision": "d8cc43e8b1cf5552bcb704cf079332a8",
+    "url": "/index.html"
+  }
+];
